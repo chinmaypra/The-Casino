fb5c2e0e7c2dacbe0b148647e6f6dcc111b3316a
chinmaypra <chinmay.s.prabhu@gmail.com>
2024-02-12 23:35:28 -0500

    modified:   .DS_Store 	new file:   Deck.java

---
+++ Deck.java
@@ -0,0 +1,8 @@
+public class Deck {
+    private String[][]deck;
+    
+    public Deck(){
+        String[][]deck1 = {{"AHeart","ADiamond","ASpaid","AClub"},{"2Heart","2Diamond","2Spaid","2Club"},
+        {"3Heart","3Diamond","3Spaid","3Club"}};
+    }
+}
